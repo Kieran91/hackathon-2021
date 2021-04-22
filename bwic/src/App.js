b37ae62873e254ab5d3f0e632819a600769096ec
James Brown <jamesbrown19921992@hotmail.com>
2021-04-22 11:45:12 +0100

    input page

--- bwic/src/App.js
+++ bwic/src/App.js
@@ -1,12 +1,55 @@
-import React from 'react';
+import React, { useReducer, useState } from 'react';
 import './App.css';
-import Bonds from "./components/Bonds";
+
+const formReducer = (state, event) => {
+ return {
+   ...state,
+   [event.name]: event.value
+ }
+}
 
 function App() {
-  
+  const [formData, setFormData] = useReducer(formReducer, {});
+  const [submitting, setSubmitting] = useState(false);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(JSON.stringify(formData))
+    setSubmitting(true);
+  }
+
+  const handleChange = event => {
+    setFormData({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+
 
   return(
-    <Bonds />
+    <div className="wrapper">
+      <h1>Insight Investment</h1>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label>
+            <p>Trades</p>
+            <input name="trade" onChange={handleChange} placeholder="Enter Bond Name"/>
+            <input name="amount" onChange={handleChange} placeholder="Enter Bond Size"/>
+          </label>
+        </fieldset>
+        <button type="submit">Submit</button>
+      </form>
+      {submitting &&
+        <div>
+        You are submitting the following:
+        <ul>
+          {Object.entries(formData).map(([name, value]) => (
+            <li key={name}><strong>{name}</strong>:{value.toString()}</li>
+          ))}
+        </ul>
+      </div>
+      }
+    </div>
   )
 }
 
