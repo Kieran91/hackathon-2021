84ebf7fd84b3e23b432cbce098bc43dcf38c8101
James Brown <jamesbrown19921992@hotmail.com>
2021-04-22 10:56:51 +0100

    second commit

--- bwic/src/App.js
+++ bwic/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Bonds from "./components/Bonds";
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          HELLO 
-        </a>
-      </header>
+      <Bonds></Bonds>
     </div>
   );
 }
